337a67ee87207d1ef51af96df904457868c40e28
Caduzca <carloseduardobrito1716@gmail.com>
2024-10-10 22:34:32 -0300

    personagens na tela

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,23 +1,29 @@
-# Example file showing a basic pygame "game loop"
+# Importa a biblioteca pygame para o script
 import pygame
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((500, 500))
-clock = pygame.time.Clock()
+# pygame configuração 
+pygame.init()# inicialização do pygame
+pygame.font.init()#inicialização do pacote de fontes no pygame
+
+
+screen = pygame.display.set_mode((500, 500)) # definição do tamanho da tela do jogo 
+clock = pygame.time.Clock() #Bliblioteca de tempo
 running = True 
 pygame.display.set_caption("Jogo Da Velha")
 
+
 cor_fundo = 1 
 #cor_fundo = 2
-
+fonte_quadrinhos = pygame.font.SysFont('Comic Sans Ms', 30)#importar fontes
+personagem_x = fonte_quadrinhos.render('X', True, 'red')
+personagem_o = fonte_quadrinhos.render('0', True, 'red')
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    # Controle de eventos 
+    # 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN: #Evento que indica que o click do mouse
             print('clicou')
             cor_fundo = cor_fundo +1     
             if (cor_fundo > 3):
@@ -25,17 +31,21 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     #screen.fill("blue")
-     
+   
+
     # RENDER YOUR GAME HERE
     if cor_fundo == 1:
-        screen.fill('blue')
+        screen.fill('black')
+        screen.blit(personagem_x,(250,250))
     elif cor_fundo == 2:
-        screen.fill('red')
+        screen.fill('black')
+        screen.blit(personagem_o,(250,250))
+        
     else:
-        screen.fill('purple')    
+        screen.fill('purple')  
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(60)  # limita FPS para 60
 
 pygame.quit()
