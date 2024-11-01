769ea497cb9a79c75458eaeeeab0422b4d676ccd
Caduzca <carloseduardobrito1716@gmail.com>
2024-10-31 22:24:34 -0300

    Aula 13

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -7,86 +7,126 @@ pygame.font.init()#inicialização do pacote de fontes no pygame
 
 
 screen = pygame.display.set_mode((600, 600)) # definição do tamanho da tela do jogo 
+pygame.display.set_caption("Jogo Da Velha") 
 clock = pygame.time.Clock() #Bliblioteca de tempo
-running = True 
-pygame.display.set_caption("Jogo Da Velha")
 
-rodadas = 0 
 fonte_quadrinhos = pygame.font.SysFont('Comic Sans Ms', 100)#importar fontes
+running = True
+
 personagem_x = fonte_quadrinhos.render('X', True, 'red')
 personagem_o = fonte_quadrinhos.render('O', True, 'green')
 
 jogador_atual = personagem_x #inicializa o jogo com x
 
+rodadas = 0 
+tabuleiro_desenhado = False
 cordenada_x = 0
-cordenada_y = 0 
+cordenada_y = 0
 
+
+q1 = ''
+q2 = ''
+q3 = ''
+q4 = ''
+q5 = ''
+q6 = ''
+q7 = ''
+q8 = ''
+q9 = ''
+
+def desenha_tabuleiro(espessura, cor):
+    #Desenha tabuleiro                  
+  #                                   Origem        Destino
+  #                                   ( x ,  y)  ( x , y )  
+    pygame.draw.line(screen, cor , (200, 0), (200, 600), espessura)
+    pygame.draw.line(screen, cor , (400, 0), (400, 600), espessura)
+    pygame.draw.line(screen, cor , (0, 200), (600, 200), espessura)
+    pygame.draw.line(screen, cor , (0, 400), (600, 400), espessura)
+  
+     
+   
+def faz_jogada():
+   
+    global q1, q2, q3, q4, q5, q6, q7, q8, q9
+    status = True
+    if q1 == '' and cordenada_x > 0 and cordenada_x < 200 and cordenada_y < 200:
+        screen.blit(jogador_atual,(60, 30)) #primeira 
+        q1 = jogador_atual
+    
+    elif q2 == '' and cordenada_x >= 200 and cordenada_x < 400 and cordenada_y < 200:
+        screen.blit(jogador_atual,(260, 30)) #segunda
+        q2 = jogador_atual
+    elif  q3 == '' and cordenada_x >= 400 and cordenada_y < 200:
+        screen.blit(jogador_atual,(460, 30)) #terceira
+        q3 = jogador_atual   
+    elif q4 == '' and cordenada_x < 200 and cordenada_y >= 200 and cordenada_y < 400:
+        screen.blit(jogador_atual,(60, 230)) #quarta
+        q4 = jogador_atual
+    elif q5 == '' and cordenada_x >= 200 and cordenada_x < 400 and cordenada_y >= 200 and cordenada_y < 400:
+        screen.blit(jogador_atual,(260, 230)) #quinto
+        q5 = jogador_atual
+    elif q6 == '' and cordenada_x >= 400 and cordenada_y >= 200 and cordenada_y < 400:
+        screen.blit(jogador_atual,(460, 230)) #sexto
+        q6 = jogador_atual
+    elif q7 == '' and cordenada_x < 200 and cordenada_y >= 400:
+        screen.blit(jogador_atual,(60, 430)) #setimo
+        q7 = jogador_atual
+    elif q8 == '' and cordenada_x >= 200 and cordenada_x < 400 and cordenada_y >= 400:
+        screen.blit(jogador_atual,(260, 430)) #oitavo
+        q8 = jogador_atual
+    elif q9 == '' and cordenada_x >= 400 and cordenada_y >= 400:
+        screen.blit(jogador_atual,(460, 430)) #nono
+        q9 = jogador_atual
+    else:
+        status = False
+    
+    return status        
+    
 while running:
     # Controle de eventos 
     # 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN: #Evento que indica que o click do mouse
+        if event.type == pygame.MOUSEBUTTONDOWN:
             print('clicou')
             pos = pygame.mouse.get_pos()
             print('eixo x:', pos[0])
             print('eixo y:', pos[1])
             cordenada_x = pos [0]
-            cordenada_y = pos [1]  
-            rodadas = rodadas + 1
-            if(rodadas >= 10):
+            cordenada_y = pos [1]
+            if(rodadas >= 9):
                 screen.fill('black')
                 rodadas = 0
-            
-            if rodadas != 1:
+                cordenada_x = 0
+                cordenada_y = 0
+                tabuleiro_desenhado = False
+            if(faz_jogada()):
+                rodadas = rodadas + 1
                 if jogador_atual == personagem_x:
                     jogador_atual = personagem_o
                 else:
-                    jogador_atual = personagem_x
-            else:
-                jogador_atual = personagem_x    
-                 
+                    jogador_atual = personagem_x   
 
-  #Desenha tabuleiro                  
-  #                                   Origem        Destino
-  #                                   ( x ,  y)  ( x , y )  
-    pygame.draw.line(screen, 'white', (200, 0), (200, 600), 10)
-    pygame.draw.line(screen, 'white', (400, 0), (400, 600), 10)
-    pygame.draw.line(screen, 'white', (0, 200), (600, 200), 10)
-    pygame.draw.line(screen, 'white', (0, 400), (600,  400), 10)
+    if tabuleiro_desenhado == False:
+
+        desenha_tabuleiro(20, 'blue')
+        q1 = ''
+        q2 = ''
+        q3 = ''
+        q4 = ''
+        q5 = ''
+        q6 = ''
+        q7 = ''
+        q8 = ''
+        q9 = ''
     
-        #     primeira linha   
+    if tabuleiro_desenhado == False:
+        desenha_tabuleiro(10, 'blue' )
+        tabuleiro_desenhado = True
+    
+            #     primeira linha   
      
-    if cordenada_x > 0 and cordenada_x < 200 and cordenada_y < 200:
-        screen.blit(jogador_atual,(60, 30)) #primeira 
-    
-    elif cordenada_x >= 200 and cordenada_x < 400 and cordenada_y < 200:
-         screen.blit(jogador_atual,(260, 30)) #segunda
-    
-    elif cordenada_x >= 400 and cordenada_y < 200:
-        screen.blit(jogador_atual,(460, 30)) #terceira
-    
-    elif cordenada_x < 200 and cordenada_y >= 200 and cordenada_y < 400:
-        screen.blit(jogador_atual,(60, 230)) #quarta
-    
-    elif cordenada_x >= 200 and cordenada_x < 400 and cordenada_y >= 200 and cordenada_y < 400:
-        screen.blit(jogador_atual,(260, 230)) #quinto
-    
-    elif cordenada_x >= 400 and cordenada_y >= 200 and cordenada_y < 400:
-        screen.blit(jogador_atual,(460, 230)) #sexto
-    
-    elif cordenada_x < 200 and cordenada_y >= 400:
-        screen.blit(jogador_atual,(60, 430)) #setimo
-    
-    elif cordenada_x >= 200 and cordenada_x < 400 and cordenada_y >= 400:
-        screen.blit(jogador_atual,(260, 430)) #oitavo
-    
-    elif cordenada_x >= 400 and cordenada_y >= 400:
-        screen.blit(jogador_atual,(460, 430)) #nono
-
-    
-    
     pygame.display.flip()
 
     clock.tick(60)  # limita FPS para 60
